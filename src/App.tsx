c8042d5db1951cd825fe3eb67699c48f70963720
helenryao <helenroseyao@gmail.com>
2024-12-25 15:08:12 -0500

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div className="app-container">
+      <form className="journal-form">
+      <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+
+        <button type="submit">Submit</button>
+        
+      </form>
+      
+    <div className="journal-grid">
+        <div className="journal-item">
+          <div className="journal-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+
+        <div className="journal-item">
+          <div className="journal-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+
+        <div className="journal-item">
+          <div className="journal-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+        
+        <div className="journal-item">
+          <div className="journal-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+
+      </div>
     </div>
+    </body>
+    
   );
 }
 
