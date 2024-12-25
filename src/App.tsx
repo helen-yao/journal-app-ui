e60d65fdb4703a21a9b9e693ab73888cd326f6a8
helenryao <helenroseyao@gmail.com>
2024-12-25 16:05:51 -0500

    able to add new note

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+
+import "./App.css";
+
+type Journal = {
+  id: number;
+  title: string;
+  content: string;
+};
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: ", title);
+    console.log("content: ", content);
+    setNotes([newEntry, ...notes]);
+    setTitle("");
+    setContent(""); 
+  };
+  const newEntry: Journal = {
+    id: notes.length + 1,
+    title: title,
+    content: content,
+  };
+
+  
   return (
-    <body>
-    <div className="app-container">
-      <form className="journal-form">
-      <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
+    <div>
+      <header>Write down your thoughts for the day!</header>
+      <div className="app-container">
+        <form className="journal-form" onSubmit={handleAddNote}>
+          <input
+            placeholder="Title"
+            required
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
 
-        <button type="submit">Submit</button>
+          <textarea
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
+            placeholder="Content"
+            rows={10}
+            required
+          />
+
+          <button type="submit">Submit</button>
+
+        </form>
         
-      </form>
-      
-    <div className="journal-grid">
-        <div className="journal-item">
-          <div className="journal-header">
-            <button>x</button>
+        <div>
+          <div className="journal-grid">
+            {notes.map((note) => (
+              <div className="journal-item">
+                <div className="journal-header">
+                  <button>x</button>
+                </div>
+                <h2>{note.title}</h2>
+                <p>{note.content}</p>
+              </div>
+            ))}
           </div>
-          <h2>Note Title</h2>
-          <p>Note content</p>
-        </div>
 
-        <div className="journal-item">
-          <div className="journal-header">
-            <button>x</button>
+          <div className="rating">
+            <h2>Rate:</h2>
           </div>
-          <h2>Note Title</h2>
-          <p>Note content</p>
         </div>
-
-        <div className="journal-item">
-          <div className="journal-header">
-            <button>x</button>
-          </div>
-          <h2>Note Title</h2>
-          <p>Note content</p>
-        </div>
-        
-        <div className="journal-item">
-          <div className="journal-header">
-            <button>x</button>
-          </div>
-          <h2>Note Title</h2>
-          <p>Note content</p>
-        </div>
-
       </div>
     </div>
-    </body>
-    
   );
-}
+};
 
 export default App;
